5fb7f14d4c07483b2260e5523576e9d25df8af3f
Lucas Nunes <lns2@cin.ufpe.br>
2017-07-01 10:35:01 -0300

    Add filter, deletion and info functions

---
+++ R/table_functions.R
@@ -0,0 +1,32 @@
+
+filter_factor = function(dataset, var, val) {
+    dataset[dataset[[var]] %in% val & !is.na(dataset[[var]]),]
+}
+
+
+filter_num = function(data, var, begin, end) {
+    data[data[[var]] >= begin & data[[var]] <= end & !is.na(data[[var]]),]
+}
+
+
+delete_column = function(dataset, var) {
+    dataset[var] = NULL
+    return(dataset)
+}
+
+
+get_info = function(dataset) {
+    types = colnames(dataset)
+    info = list()
+    for (t in colnames(dataset)) {
+        col = dataset[[t]]
+        if (is.numeric(col)) {
+            info[[t]] = list(type = "numeric", values = c(min(col, na.rm = TRUE), max(col, na.rm = TRUE)))
+
+        } else if(is.factor(col)) {
+            info[[t]] = list(type = "factor", values = levels(col))
+        }
+    }
+
+    return(info)
+}
