2624bd3435a81c1ea71ee630fa37f0b0acb75c2a
Lucas Nunes <lns2@cin.ufpe.br>
2017-07-01 13:30:47 -0300

    outlier: remove plot and add info return

--- R/outlier.R
+++ R/outlier.R
@@ -7,8 +7,7 @@ outlier_boxplot = function(dataset, col, rm_na = TRUE) {
     bp = boxplot(dataset[[col]])
     out = data.frame(bp$out)
     names(out) = col
-
-    list(plot = bp, outliers = out)
+    return(out)
 }
 
 
@@ -16,8 +15,7 @@ outlier_bagplot = function(dataset, col1, col2) {
     bp = aplpack::bagplot(dataset[[col1]], dataset[[col2]])
     out = data.frame(bp$pxy.outlier)
     names(out) = c(col1, col2)
-
-    list(plot = bp, outliers = out)
+    return(out)
 }
 
 
@@ -25,7 +23,7 @@ outlier_bv_boxplot = function(dataset, col1, col2) {
     bp = asbio::bv.boxplot(dataset[[col1]], dataset[[col2]])
     out = bp$out
     names(out) = c(col1, col2)
-    list(plot = bp, outliers = out)
+    return(out)
 }
 
 
@@ -47,7 +45,10 @@ univariate_outliers = function(dataset, var, type = "boxplot", rm_na = TRUE) {
         dataset = dataset[!(is.na(dataset[[var]])),]
     }
 
-    univar_func[[type]](dataset, var)
+    info = ""
+    out = univar_func[[type]](dataset, var)
+
+    return(list(outliers = out, info = info))
 }
 
 
@@ -58,7 +59,10 @@ bivariate_outliers = function(dataset, var1, var2, type = "bvboxplot", rm_na = T
         dataset = dataset[!(is.na(dataset[[var1]])) & !(is.na(dataset[[var2]])),]
     }
 
-    bivar_func[[type]](dataset, var1, var2)
+    info = ""
+    out = bivar_func[[type]](dataset, var1, var2)
+
+    return(list(info = info, outliers = out, info = info))
 }
 
 
